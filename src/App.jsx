3b8c0d64a1ea57353a91cbc077719691880b9c78
Lokesh Ghosh <lghosh857@gmail.com>
2023-12-01 15:56:22 +0530

    update : project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <h1 className='text-red-500 text-5xl text-center'>👷🏿‍♂️Project under Construction 🚧</h1>
+  )
+}
+
+export default App
